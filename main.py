1e1f130a0722987dd28ddfc67b1da6b8a7e1914f
haroonfayyaz17 <haroonfayyaz17@gmail.com>
2024-01-14 16:19:44 +0500

    cors issue resolved

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import auth, user
 from app.database import Base, engine
 from pydantic import BaseModel
@@ -8,6 +9,16 @@ import app.auth.auth_handler
 
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(user.router)
